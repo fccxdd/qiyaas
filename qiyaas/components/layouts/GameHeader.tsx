1e6435a082ab77215ae7672edd9b07ed726ba9cc
Ms Teema <queentima831@gmail.com>
2025-10-21 17:34:51 -0400

    Game Header

---
+++ qiyaas/components/layouts/GameHeader.tsx
@@ -0,0 +1,17 @@
+// components/layouts/GameHeader.tsx
+
+import { ReactNode } from 'react';
+
+interface GameHeaderProps {
+  leftContent: ReactNode;
+  rightContent: ReactNode;
+}
+
+export default function GameHeader({ leftContent, rightContent }: GameHeaderProps) {
+  return (
+    <header className="absolute top-8 left-8 right-8 sm:top-20 sm:left-20 sm:right-20 z-50 flex items-center justify-between">
+      {leftContent}
+      {rightContent}
+    </header>
+  );
+}
