dc9f9c52b6af22fa2b89f098078e8c19f7ecaa77
Ms Teema <queentima831@gmail.com>
2025-11-16 07:16:36 -0500

    making the game header positioning fixed

--- qiyaas/components/layouts/GameHeader.tsx
+++ qiyaas/components/layouts/GameHeader.tsx
@@ -9,7 +9,7 @@ interface GameHeaderProps {
 
 export default function GameHeader({ leftContent, rightContent }: GameHeaderProps) {
   return (
-    <header className="absolute top-0 left-0 right-0 z-50 flex items-center justify-between border-b border-gray-200 dark:border-gray-500 pb-1.5 sm:pb-4 px-4 sm:px-20 pt-3 sm:pt-2">
+    <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between border-b border-gray-200 dark:border-gray-500 pb-1.5 sm:pb-4 px-4 sm:px-20 pt-3 sm:pt-2">
       {leftContent}
       {rightContent}
     </header>
