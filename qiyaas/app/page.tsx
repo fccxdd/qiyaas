c096e47df7448949a19effe36e15e8b9442adeab
Ms Teema <queentima831@gmail.com>
2026-02-21 09:52:43 -0500

    deleting lightmode from homepage and 404 page

--- qiyaas/app/page.tsx
+++ qiyaas/app/page.tsx
@@ -1,4 +1,3 @@
-import ThemeToggle from "@/components/game_assets/settings/ThemeToggle";
 import EmailButton from "@/components/contact/EmailButton";
 import QiyaasLogo from "@/components/ux/QiyaasLogo";
 import { GameConfig } from "@/lib/gameConfig";
@@ -48,11 +47,7 @@ export default async function Home() {
   
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 relative">
-      
-      <header className="row-start-1 self-start w-full flex justify-end">
-        <ThemeToggle />
-      </header>
-      
+           
       <main className="flex flex-col gap-8 sm:gap-10 row-start-2 items-center">
         
         <QiyaasLogo className="w-[200px] h-[100px] sm:w-[400px] sm:h-[193px]"/>
